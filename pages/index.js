c30c0aebaf0325f2604d948a23dcd1dc90f83b34
Michael Prather <mprather1@gmail.com>
2018-07-26 15:46:21 +0000

    just copied and pasted a bunch of stuff - works now

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
+import { getPosts } from '../api/posts'
 import Layout from '../layouts/Main'
 
-export default () =>
+import Post from '../components/Post'
+
+const IndexPage = ({ posts }) =>
   <Layout>
-    <h1>Home!</h1>
+    <ul>
+      {posts.map(p => <Post key={p.title} post={p} />)}
+    </ul>
   </Layout>
+
+IndexPage.getInitialProps = async ({ req }) => {
+  const res = await getPosts()
+  const json = await res.json()
+  return { posts: json }
+}
+
+export default IndexPage
