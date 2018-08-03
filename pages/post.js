eea6e7556ae766b2efaa7e2b974dd53d69825496
Michael Prather <mprather1@gmail.com>
2018-08-03 05:24:28 +0000

    fetch or get posts from store

--- pages/post.js
+++ pages/post.js
@@ -47,9 +47,16 @@ const PostPage = ({ post }) =>
     </Wrapper>
   </Layout>
 
-PostPage.getInitialProps = async ({ query }) => {
-  let res = await getPost(query.slug)
-  let post = await res.json()
+PostPage.getInitialProps = async ({ query, reduxStore }) => {
+  let post
+
+  if (reduxStore.getState().posts.data) {
+    post = reduxStore.getState().posts.data.find(e => e.id === parseInt(query.slug))
+  } else {
+    let res = await getPost(query.slug)
+    post = await res.json()
+  }
+
   return { post }
 }
 
