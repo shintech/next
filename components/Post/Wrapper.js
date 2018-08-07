c2e4fe09130f9961a5774761f5f7bf348d3e0b9e
Michael Prather <mprather1@gmail.com>
2018-08-07 04:08:55 +0000

    trying some different fonts

--- components/Post/Wrapper.js
+++ components/Post/Wrapper.js
@@ -8,6 +8,7 @@ const Wrapper = styled.article`
   grid-template-rows: auto auto;
   
   h1 {
+    font-family: "Times New Roman", Times, serif;
     grid-row: 1;  
     grid-column: span 2;
     border-bottom: 4px dotted teal;
@@ -16,28 +17,28 @@ const Wrapper = styled.article`
     font-weight: bold;  
     color: #222;
     line-height: 2.5ch;
-    font-family: "PT Sans", sans-serif;
   }
   
   aside {
     margin-top: 15px;
     grid-row: 2;  
     grid-column: 2;  
-    font-family: "PT Serif", sans-serif;
     border: 1px solid teal;
     padding: 1ch;
-    
+    font-family: "Times New Roman", Times, serif;
+   
     :hover {
-      color: darkslategray;
+      color: indianred;
       cursor: default;
     }
   }
   
   p {
     padding: 0 2ch 0 2ch;
-    color: #444;
     grid-row: span 2;  
-    grid-column: 1;      
+    grid-column: 1;    
+    font-family: "curier new", courier, monospace;
+
   }
 `
 
