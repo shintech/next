18f537958792ac03371715f403da2898524e3723
Michael Prather <mprather1@gmail.com>
2018-08-13 00:01:40 +0000

    update component/Grid/Wrapper

---
+++ api/accounts.js
@@ -0,0 +1,12 @@
+/*  /api/about.js
+*/
+import fetch from 'isomorphic-fetch'
+
+export function fetchAccounts () {
+  return fetch('http://dev.shintech.ninja:8000/api/accounts', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
--- components/Grid/Wrapper.js
+++ components/Grid/Wrapper.js
@@ -1,39 +1,46 @@
-/*   /components/Flex/Wrapper.js
+/*   /components/Grid/Wrapper.js
 */
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
   display: grid;
-  grid: 1fr 1fr 1fr 1fr / 1fr 1fr 1fr 1fr;
+  grid-template-rows: [start row-1] 1fr [row-2] 1fr [row-3] 1fr [row-4] 1fr [stop end];
+  grid-template-columns:  [start col-1] 1fr [col-2]  1fr [col-3]  1fr [col-4] 1fr [stop end last];
+
+  min-height: 12ch;
   padding: 1ch;
   
   div {
     text-align: center;
+    :hover {
+      border: 3px solid white;
+      cursor: default;
+    }
   }
   
   .div-1 {
-    grid-row: 1 / span 4;
-    grid-column: 4;
+    grid-row: start / span end;
+    grid-column: col-4 / last;
     background-color: red;
   }
   
   
   .div-2 {
-    grid-row: 2 / span 4;
-    grid-column: 3;
+    grid-row: row-2 / span end;
+    grid-column: col-3;
     background-color: blue;
   }
   
   
   .div-3 {
-    grid-row: 3 / span 4;
-    grid-column: 2;
+    grid-row: row-3 / span end;
+    grid-column: col-2;
     background-color: green;
   }  
   
   .div-4 {
-    grid-row: 4;
-    grid-column: 1;
+    grid-row: row-4;
+    grid-column: start;
     background-color: orange;
   }    
 `
