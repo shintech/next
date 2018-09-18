5c75342e3d35115288667b257a9b92ed699c8afc
Michael Prather <mprather1@gmail.com>
2018-09-18 06:04:30 +0000

    jest and tests for other endpoints

--- __tests__/index.js
+++ __tests__/index.js
@@ -4,9 +4,24 @@ import { shallow } from 'enzyme'
 import React from 'react'
 
 import Nav from '../components/Nav'
+import Post from '../components/Post'
 
 describe('Next', () => {
-  it('Navbar test..."', () => {
+  it('Navbar...', () => {
     expect(shallow(<Nav pathname='/' />).find('nav').length).toEqual(1)
+  })
+
+  it('Post test...', () => {
+    let attrs = {
+      title: 'title',
+      body: 'body',
+      summary: 'summary'
+    }
+
+    let post = shallow(<Post post={attrs} />)
+
+    expect(post.find('h1').text()).toEqual('title')
+    expect(post.find('p').text()).toEqual('body')
+    expect(post.find('aside').text()).toEqual('summary')
   })
 })
--- components/Post/index.js
+++ components/Post/index.js
@@ -1,5 +1,6 @@
 /*   /components/Post/index.js
 */
+import React from 'react'
 import PropTypes from 'prop-types'
 import Wrapper from './Wrapper'
 
