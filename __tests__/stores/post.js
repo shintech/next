a9ad925e65a1f782dc225df5132a51ca07c600f1
Michael Prather <mprather1@gmail.com>
2018-09-26 00:09:51 +0000

    added _posts to jest globals

--- __tests__/stores/post.js
+++ __tests__/stores/post.js
@@ -11,21 +11,9 @@ const store = initStore({}, {
   isServer: true
 })
 
-let fakeResponse = [
-  {
-    _id: '5b9eeda95744c570b3689035',
-    title: 'possimus molestiae mollitia',
-    body: 'Architecto nesciunt quidem. Modi ut a aut. Nobis est occaecati quia vitae officia sed adipisci ab odio. Aut enim et. Qui et error ipsum atque ut molestiae reprehenderit dolorum explicabo. Repudiandae dolor fugit non.\n \rEst sapiente voluptatem nihil et magnam et. Quis non et iure. Minima perspiciatis ducimus ab quo aliquid. Fuga molestias et cupiditate nesciunt qui possimus. Delectus asperiores nisi ut dolores ab rerum. Minima vero sit blanditiis impedit et.\n \rEt occaecati est rem quasi. Repellat aut consequatur aut. Quo quas assumenda voluptas cum placeat nam deserunt id. In sed in. Nisi pariatur accusamus animi sequi.',
-    summary: 'Labore ipsum sed maxime qui iusto voluptatem quia.'
-  }
-]
-
 nock('https://shintech.ninja')
-  .get('/api/posts')
-  .reply(200, fakeResponse)
-
   .get('/api/posts/5b9eeda95744c570b3689035')
-  .reply(200, fakeResponse[0])
+  .reply(200, _posts[0])
 
 describe('fetchPost...', () => {
   beforeAll(async () => {
@@ -36,18 +24,18 @@ describe('fetchPost...', () => {
   })
 
   it('expect post to have property _id...', () => {
-    expect(store.getState().post.data._id).toEqual(fakeResponse[0]._id)
+    expect(store.getState().post.data._id).toEqual(_posts[0]._id)
   })
 
   it('expect post to have property title...', () => {
-    expect(store.getState().post.data.title).toEqual(fakeResponse[0].title)
+    expect(store.getState().post.data.title).toEqual(_posts[0].title)
   })
 
   it('expect post to have property body...', () => {
-    expect(store.getState().post.data.body).toEqual(fakeResponse[0].body)
+    expect(store.getState().post.data.body).toEqual(_posts[0].body)
   })
 
   it('expect post to have property summary...', () => {
-    expect(store.getState().post.data.summary).toEqual(fakeResponse[0].summary)
+    expect(store.getState().post.data.summary).toEqual(_posts[0].summary)
   })
 })
--- jest.setup.js
+++ jest.setup.js
@@ -4,12 +4,13 @@ import React from 'react'
 
 global.React = React
 
-global.getConfig = () => {
-  return {
-    publicRuntimeConfig: {
-      hostname: 'localhost:8000'
-    }
+global._posts = [
+  {
+    _id: '5b9eeda95744c570b3689035',
+    title: 'possimus molestiae mollitia',
+    body: 'Architecto nesciunt quidem. Modi ut a aut. Nobis est occaecati quia vitae officia sed adipisci ab odio. Aut enim et. Qui et error ipsum atque ut molestiae reprehenderit dolorum explicabo. Repudiandae dolor fugit non.\n \rEst sapiente voluptatem nihil et magnam et. Quis non et iure. Minima perspiciatis ducimus ab quo aliquid. Fuga molestias et cupiditate nesciunt qui possimus. Delectus asperiores nisi ut dolores ab rerum. Minima vero sit blanditiis impedit et.\n \rEt occaecati est rem quasi. Repellat aut consequatur aut. Quo quas assumenda voluptas cum placeat nam deserunt id. In sed in. Nisi pariatur accusamus animi sequi.',
+    summary: 'Labore ipsum sed maxime qui iusto voluptatem quia.'
   }
-}
+]
 
 configure({ adapter: new Adapter() })
