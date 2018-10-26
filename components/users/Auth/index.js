cc17ee46cbdf19f4b1425833f899e6631296de04
Michael Prather <mprather1@gmail.com>
2018-10-26 02:31:15 +0000

    lint

--- components/users/Auth/index.js
+++ components/users/Auth/index.js
@@ -18,33 +18,32 @@ const LoginForm = (props) => {
 
   return (
     <Wrapper>
-      <div>{(authorized) ? <div>Authorized: <span className='true'>true</span></div> :
-      <form onSubmit={submit}>
-        <div>Authorized: <span className='false'>false</span></div>      
-        <ul className='form-style'> 
-          <li className='form-group'>
-            <label>Username <span className="required">*</span></label>
-            <input ref={input => _username = input} type='text' placeholder='Username' required />
-          </li>
-          
-          <li className='form-group'>
-            <label>Password<span className="required">*</span></label>
-            <input ref={input => _password = input} type='password' placeholder='Password' required />
-          </li>
-          
-          <li className='form-group'>
-            <button type="submit" className="submit">Submit</button>
-          </li>
-          <li>
-          </li>
-        </ul>
-      </form>
+      <div>{(authorized) ? <div>Authorized: <span className='true'>true</span></div>
+        : <form onSubmit={submit}>
+          <div>Authorized: <span className='false'>false</span></div>
+          <ul className='form-style'>
+            <li className='form-group'>
+              <label>Username <span className='required'>*</span></label>
+              <input ref={input => _username = input} type='text' placeholder='Username' required />
+            </li>
+
+            <li className='form-group'>
+              <label>Password<span className='required'>*</span></label>
+              <input ref={input => _password = input} type='password' placeholder='Password' required />
+            </li>
+
+            <li className='form-group'>
+              <button type='submit' className='submit'>Submit</button>
+            </li>
+            <li />
+          </ul>
+        </form>
       }</div>
     </Wrapper>
   )
+}
 
   /* eslint-enable */
-}
 
 LoginForm.propTypes = {
   login: PropTypes.func.isRequired,
