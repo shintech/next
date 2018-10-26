4876e258917faedf2d4a56fe6ab7e851bb834a43
Michael Prather <mprather1@gmail.com>
2018-10-26 02:28:33 +0000

    form auth message styling sorta

--- components/users/Auth/Wrapper.js
+++ components/users/Auth/Wrapper.js
@@ -3,6 +3,13 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.ul`
+  .true {
+    color: green;
+  }
+  
+  .false {
+    color: red;
+  }
 `
 
 export default Wrapper
--- components/users/Auth/index.js
+++ components/users/Auth/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-// import Wrapper from './Wrapper'
+import Wrapper from './Wrapper'
 
 const LoginForm = (props) => {
   let _username, _password
@@ -17,27 +17,30 @@ const LoginForm = (props) => {
   /* eslint-disable */
 
   return (
-    <form onSubmit={submit}>
-      <ul className='form-style'> 
-        <li className='form-group'>
-          <label>Username <span className="required">*</span></label>
-          <input ref={input => _username = input} type='text' placeholder='Username' required />
-        </li>
-        
-        <li className='form-group'>
-          <label>Password<span className="required">*</span></label>
-          <input ref={input => _password = input} type='password' placeholder='Password' required />
-        </li>
-        
-        <li className='form-group'>
-          <button type="submit" className="submit">Submit</button>
-        </li>
-        <li>
-          <p>Authorized: {(authorized) ? <strong>true</strong> : <strong>false</strong> }</p>
-        </li>
-      </ul>
-
-    </form>
+    <Wrapper>
+      <div>{(authorized) ? <div>Authorized: <span className='true'>true</span></div> :
+      <form onSubmit={submit}>
+        <div>Authorized: <span className='false'>false</span></div>      
+        <ul className='form-style'> 
+          <li className='form-group'>
+            <label>Username <span className="required">*</span></label>
+            <input ref={input => _username = input} type='text' placeholder='Username' required />
+          </li>
+          
+          <li className='form-group'>
+            <label>Password<span className="required">*</span></label>
+            <input ref={input => _password = input} type='password' placeholder='Password' required />
+          </li>
+          
+          <li className='form-group'>
+            <button type="submit" className="submit">Submit</button>
+          </li>
+          <li>
+          </li>
+        </ul>
+      </form>
+      }</div>
+    </Wrapper>
   )
 
   /* eslint-enable */
