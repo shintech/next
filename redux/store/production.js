59490c23c09b0d1e34c64fc3d40d299b466d8fec
Michael Prather <mprather1@gmail.com>
2018-11-05 01:52:32 +0000

    removed redux-devtools from production build

--- redux/store/production.js
+++ redux/store/production.js
@@ -1,21 +1,19 @@
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'remote-redux-devtools'
 import withRedux from 'next-redux-wrapper'
 import thunk from 'redux-thunk'
 import reducers from '../reducers'
 
-const logger = store => next => action => {
-  console.log(`dispatching action ${action.type}...`)
-  return next(action)
-}
+// const logger = store => next => action => {
+//   console.log(`dispatching action ${action.type}...`)
+//   return next(action)
+// }
 
 const middleWare = () => [
-  thunk,
-  logger
+  thunk
 ]
 
 export const initStore = (initialState = {}, c) => {
-  return createStore(reducers, initialState, composeWithDevTools(applyMiddleware(...middleWare())))
+  return createStore(reducers, initialState, applyMiddleware(...middleWare()))
 }
 
 export const reduxPage = (comp) => withRedux(initStore)(comp)
